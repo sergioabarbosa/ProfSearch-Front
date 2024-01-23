cc9cb82ef1c05514f9391fd89d68fc8022b3ecbf
Sergio Barbosa <sergio.alvesbarbosa@gmail.com>
2024-01-23 17:57:31 -0300

    add SearchPage

--- pages/SearchPage.js
+++ pages/SearchPage.js
@@ -1,14 +1,31 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet, FlatList, Image } from 'react-native';
+import api from '../api';
 
 const SearchPage = () => {
   const [searchQuery, setSearchQuery] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
 
-  const handleSearch = () => {
-    // Lógica de busca aqui
-    console.log('Realizando busca por:', searchQuery);
-    // Implemente a lógica de busca conforme necessário
-  };
+  const handleSearch = async () => {
+  try {
+    const response = await api.get(`/anuncios/search?q=${searchQuery}`);
+    const data = response.data;
+
+    // Verifique se há dados e se é um array
+    const anunciosRecuperados = data.data;
+    if (anunciosRecuperados && Array.isArray(anunciosRecuperados)) {
+      // Extrai apenas os dados do documento _doc
+      const anunciosData = anunciosRecuperados.map((anuncio) => anuncio._doc);
+      console.log('Anúncios recuperados:', anunciosData);
+
+      // Atualiza o estado com os dados filtrados
+      setSearchResults(anunciosData);
+    }
+  } catch (error) {
+    console.error('Erro ao buscar anúncios:', error.message);
+  }
+};
+
 
   return (
     <View style={styles.container}>
@@ -20,6 +37,19 @@ const SearchPage = () => {
         onChangeText={(text) => setSearchQuery(text)}
       />
       <Button title="Buscar" onPress={handleSearch} />
+
+      <FlatList
+        data={searchResults}
+        keyExtractor={(item) => item._id}
+        renderItem={({ item }) => (
+          <View style={styles.resultContainer}>
+            <Text style={styles.resultTitle}>{item.title}</Text>
+            <Text style={styles.resultDescription}>{item.description}</Text>
+            {item.image && <Image source={{ uri: item.image }} style={styles.resultImage} />}
+            {/* Adicione outros detalhes do anúncio conforme necessário */}
+          </View>
+        )}
+      />
     </View>
   );
 };
@@ -43,6 +73,28 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     marginBottom: 20,
     paddingHorizontal: 10,
+  },
+  resultContainer: {
+    marginVertical: 10,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 8,
+  },
+  resultTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  resultDescription: {
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  resultImage: {
+    width: 200,
+    height: 200,
+    resizeMode: 'cover',
+    marginBottom: 5,
   },
 });
 
